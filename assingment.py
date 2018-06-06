24ce99c0f0adeeda1fec96fb7c06309aed6733f0
vikramjitsingh0001 <vikramjitsinghkarwal@gmail.com>
2018-06-07 00:37:14 +0530

    First Commit

---
+++ assingment.py
@@ -0,0 +1,27 @@
+#Q1
+print("hi hello kida g")
+#output: # hi hello kida g
+#Q2
+print("vikramjit"+"singh")
+#output: vikramjitsingh
+#Q3
+a,b,c=8,5,4
+print(a, b, c)
+#output: 8 5 4
+#Q4
+print('let\'s get started')
+#output: let's get started
+#Q5
+s="acadview"
+course="python"
+fees=5000
+print("hi your center is %s your course is %s and your course fees is Rs %d"%(s,course,fees))
+#output: hi your center is acadview your course is python and your course fees is Rs 5000
+#Q6
+name="tony stark"
+salary=100000
+print("hello mr %s we will provide you Rs%d as salary for your job"%(name,salary))
+#output: hello mr tony stark we will provide you Rs100000 as salary for your job
+#if this then there is a error
+print('hello mr %s we will provide you Rs%d as salary for your job')%(name,salary)
+#output: so error
