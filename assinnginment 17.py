c5d6cbcdeaddad03df27e2fb7a0cd7d3a6da7502
Vikramjitsingh0001 <39943943+Vikramjitsingh0001@users.noreply.github.com>
2018-06-28 00:56:47 +0530

    aassinginment 17

---
+++ assinnginment 17.py
@@ -0,0 +1,30 @@
+#Q1
+import tkinter
+top = tkinter.Tk()
+
+top.title("tiger zinda ha")
+top.geometry("300x300")
+from tkinter import *
+Lb = Listbox(top)
+Lb.insert(1,'hello word')
+Lb.pack()
+btn = tkinter.Button(top,text ="button",command = "tiger")
+
+
+btn.pack()
+
+top.mainloop()
+
+'''
+
+#Q2
+import tkinter
+top = tkinter.Tk()
+
+top.title("tiger zinda ha")
+top.geometry("300x300")
+btn = tkinter.Button(top,text ="button",command = "tiger")
+btn.pack()
+
+top.mainloop()
+'''
